3bebe5f76917a8cde87cb832713c21d10b7ee68a
Mark <markjackson2112@gmail.com>
2021-02-07 01:44:28 -0500

    added a buttload of comments to improve readability.

--- Project-Creator/Project-Creator/Classes/Project.cs
+++ Project-Creator/Project-Creator/Classes/Project.cs
@@ -1,46 +1,101 @@
-﻿using System;
+﻿/*
+*	FILE : Project.cs
+*	PROJECT : Project Creator
+*	PROGRAMMER : Mark Jackson
+*	             Eric Emerson
+*	FIRST VERSION : 2021-02-06
+*	DESCRIPTION :
+*		This class contains the defintion of a project based on the requirements of Project Creator.
+*		Currentlty, a project contains a title, author, an icon, a navigation url and, a list of updates
+*		This will definately change in the future as the scope changes and especially once we have a 
+*		better grasp on inter page navigation
+*/
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace Project_Creator
-{
-    public class Project
-    {
-        public string title { get; set; }
-        public string author { get; set; }
-        public string icon { get; set; }
-        public string url { get; set; }
-        public ArrayList updates;
+namespace Project_Creator {
+    public class Project {
+        public string title { get; set; }       /*the name of the project*/
+        public string author { get; set; }      /*who the project belongs to*/
+        public string icon { get; set; }        /*an image describing the project*/
+        public string url { get; set; }         /*the navigation url for the project. currently used when a project is in the recommended section*/
+        public ArrayList updates;               /*a list containing all of the Update objects associated with the project*/
 
-        public Project(string t, string a, string i, string URL, ArrayList u)
-        {
+
+        /*
+        * METHOD : Project -- Constructor
+        * DESCRIPTION :
+        *	create a new project with all of the members specified by the caller
+        * PARAMETERS :
+        *	string t : the title of the project
+        *   string a : the author of the project
+        *   string i : the URL for the icon
+        *   string URL : the URL that the project exists on
+        *   ArrayList u : an arraylist that contains all of the updates for the project
+        * RETURNS :
+        *	none
+        *	A project object is created
+        */
+        public Project(string t, string a, string i, string URL, ArrayList u) {
             title = t;
             author = a;
             icon = i;
             updates = u;
             url = URL;
         }
-        public Project(string t, string a, string i, string URL)
-        {
+
+
+
+        /*
+        * METHOD : Project -- Constructor
+        * DESCRIPTION :
+        *	create a new project with all without the update list but with all other menbers
+        * PARAMETERS :
+        *	string t : the title of the project
+        *   string a : the author of the project
+        *   string i : the URL for the icon
+        *   string URL : the URL that the project exists on
+        * RETURNS :
+        *	none
+        *	A project object is created
+        */
+        public Project(string t, string a, string i, string URL) {
             title = t;
             author = a;
             icon = i;
             url = URL;
+
+            /*create a list for the updates and get them from the db*/
             updates = new ArrayList();
             getUpdates();
         }
 
-        public bool getUpdates()
-        {
+
+
+        /*
+        * METHOD : getUpdates
+        * DESCRIPTION :
+        *	fetch the updates for the project from the database and put them into the updates data member
+        * PARAMETERS :
+        *	none
+        * RETURNS :
+        *	bool : true if the data was sucessfully retrieved
+        *	       false if retrieval failed
+        *	updates data member is changed
+        */
+        public bool getUpdates() {
             bool success = false;
-            updates.Clear();
+
+            /*this whole method needs to change when we connect it to the database*/
+
+            updates.Clear();    /*remove all of the contents in the array list
             /*get from db*/
 
             /*temp data for now*/
-            for (int i = 0; i < 10; i++)
-            {
+            for (int i = 0; i < 10; i++) {
                 Update u = new Update("https://cdn.escapistmagazine.com/media/global/images/library/deriv/1400/1400821.jpg", "https://google.com", "<h3>Project Update Title</h3>", "<p>a description about the update which summarises the complete description that is displayed upon clicking on the card for more information.</p><br />", DateTime.Today);
                 updates.Add(u);
             }
--- Project-Creator/Project-Creator/ProjectManagement/ProjectTimeline.aspx.cs
+++ Project-Creator/Project-Creator/ProjectManagement/ProjectTimeline.aspx.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿/*
+*	FILE : ProjectTimeline.aspx.cs
+*	PROJECT : Project Creator
+*	PROGRAMMER : Mark Jackson
+*	FIRST VERSION : 2021-02-06
+*	DESCRIPTION :
+*		This webpage displays a short version of all of the project posts.
+*		things it does.
+*		-build and display details from each update*
+*		-build and display some related projects*
+*		things to do
+*		-get project information via a parameter
+*		-connect and read data from the database
+*		-differentiate the project owner from viewers
+*/
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,17 +23,39 @@ using System.Web.UI.WebControls;
 
 namespace Project_Creator.ProjectManagement {
     public partial class ProjectTimeline : System.Web.UI.Page {
-        string user = "user";
+        string user = "user";   /*potentally gotten from somewhere. maybe the session*/
+
+        /*this is the project that we are viewing. currently has place holder info. should be gotten from the previous screen*/
+        /*may be pass as a string. if that is the case we need to reteieve from the database. we will see*/
+        Project project = new Project("Test Project", "Mark", "https://64.media.tumblr.com/avatar_4f1f4a829fdd_128.pnj", "https://google.com");
+
+
+        /*
+        * METHOD : Page_Load
+        * DESCRIPTION :
+        *	when the page is first loaded, populate the Update panel with updates for the project
+        * PARAMETERS :
+        *	object sender : the object that invoked the method
+        *   EventArgs e : addional arguments thata may be useful
+        * RETURNS :
+        *	none
+        */
         protected void Page_Load(object sender, EventArgs e) {
-            Project project = new Project("Test Project", "Mark", "https://64.media.tumblr.com/avatar_4f1f4a829fdd_128.pnj", "https://google.com");
-            projectIcon.ImageUrl = project.icon;
-            lblTitle.Text = project.title;
-            lblAuthor.Text = project.author;
+            projectIcon.ImageUrl = project.icon;    /*set the icon to be the icon stored within the project*/
+            lblTitle.Text = project.title;          /*update the title label to the project name*/
+            lblAuthor.Text = project.author;        /*update the author label with data from the project object*/
+
+            /*for each of the updates within the project*/
             foreach (Update u in project.updates) {
+                /*create a div using createUpdatePanel and add it to the UpdatePanel section of the screen*/
                 Panel div = createUpdatePanel(u);
                 UpdatePanel.Controls.Add(div);
             }
+
+            /*there are some additional options if the user is the owner of the project, in that case, they can create a new post*/
+            /*we need to figure out how the user is logged in.*/
             if(user == "owner") {
+                /*create a new update object with a + logo and add it to the screen*/
                 Update newUpdate = new Update("https://media.istockphoto.com/vectors/black-plus-sign-positive-symbol-vector-id688550958?k=6&m=688550958&s=612x612&w=0&h=nVa-a5Fb79Dgmqk3F00kop9kF4CXFpF4kh7vr91ERGk=", "https://github.com/", " < h3>New Update</h3> <br />", "<p />", DateTime.Today);
                 Panel p = createUpdatePanel(newUpdate);
                 UpdatePanel.Controls.Add(p);
@@ -27,56 +64,95 @@ namespace Project_Creator.ProjectManagement {
 
 
             /*generate related projects this will come from the db later*/
+            /*create 4 projects to show at the bottom of the page*/
+            /*they are all just the test project currently*/
+            /*format the title to fit nicely on the screen. should probably be done differently. more research is needed*/
             project.title = "<h3>" + project.title + "</h3>";
             project.author = "<p>" + project.author + "</p>";
             ArrayList otherProjects = new ArrayList();
             for (int i = 0; i < 4; i++) {
-                otherProjects.Add(project);
+                otherProjects.Add(project); 
             }
 
+            /*for each of the projects in the related projects list*/
             foreach(Project p in otherProjects) {
+                /*create a div using createUpdatePanel and add it to the related panel of the screen*/
                 Panel div = createRelatedPanel(p);
                 RelatedPanel.Controls.Add(div);
             }
 
         }
 
-        public Panel createRelatedPanel(Project project) {
-            Panel div = new Panel();
-            div.CssClass = "w3-quarter";
-            HyperLink image = new HyperLink();
-            image.NavigateUrl = project.url;
 
-            //Image image = new Image();
-            Label title = new Label();
-            Label author = new Label();
-            image.ImageUrl = project.icon;
-            image.CssClass = "related-img";
-            title.Text = project.title;
-            author.Text = project.author;
+
+        /*
+        * METHOD : createRelatedPanel
+        * DESCRIPTION :
+        *	each of the projects in the related panel need to be dynamically generated. that is what this does.
+        *	it builds a div with an image, the author of the project and, the title
+        * PARAMETERS :
+        *	Project project : the project that we want to add
+        * RETURNS :
+        *	Panel : a div containing all of the data relevant for a related project
+        */
+        public Panel createRelatedPanel(Project project) {
+            Panel div = new Panel();            /*the div we are building*/
+            HyperLink image = new HyperLink();  /*this is the image that is used to diplay the project as well as navigate to that project*/
+            Label title = new Label();          /*the label that will hold the title of the project*/
+            Label author = new Label();         /*the label that will hold the author of the project*/
+
+            div.CssClass = "w3-quarter";        /*the styling that is used for formatting the div*/
+
+            image.NavigateUrl = project.url;    /*set the location we want the image to take us. it is stored within the Project object*/
+            image.ImageUrl = project.icon;      /*set the image we want to use for the hyperlink. it is stored within the Project object*/
+            image.CssClass = "related-img";     /*the styling that is used for formatting the image*/
+
+            title.Text = project.title;         /*set the title of the project based on the project object*/
+            author.Text = project.author;       /*set the author of the project based on the project object*/
+
+            /*add the objects we created to the div in the order we want them to appear*/
             div.Controls.Add(image);
             div.Controls.Add(title);
             div.Controls.Add(author);
+
             return (div);
         }
 
+
+
+        /*
+        * METHOD : createUpdatePanel
+        * DESCRIPTION :
+        *	each of the updates in the updates panel need to be dynamically generated. that is what this does.
+        *	it builds a div with an image, the update title, a breif update description and, the date of the update
+        * PARAMETERS :
+        *	Update update : the update we want to add
+        * RETURNS :
+        *	Panel : a div containing all of the data relevant for a related project
+        */
         public Panel createUpdatePanel(Update update) {
-            Panel div = new Panel();
-            div.CssClass = "w3-quarter";
-            HyperLink image = new HyperLink();
-            image.NavigateUrl = update.updatePage;
-            Label updateTitle = new Label();
-            Label updateDescription = new Label();
-            Label updateDate = new Label();
-            image.ImageUrl = update.iconURL;
-            image.CssClass = "update-img";
-            updateTitle.Text = update.title;
-            updateDescription.Text = update.description;
-            updateDate.Text = update.date.ToString("d");
+            Panel div = new Panel();                /*the div we are building*/
+            HyperLink image = new HyperLink();      /*this is the image that is used to diplay the project as well as navigate to that update*/
+            Label updateTitle = new Label();        /*the label that will hold the title of the update*/
+            Label updateDescription = new Label();  /*the label that will hold the description of the update*/
+            Label updateDate = new Label();         /*the label that will hold the date of the update*/
+
+            div.CssClass = "w3-quarter";             /*the styling that is used for formatting the div*/
+
+            image.NavigateUrl = update.updatePage;  /*set the location we want the image to take us. it is stored within the Update object*/
+            image.ImageUrl = update.iconURL;        /*set the image we want to use for the hyperlink. it is stored within the Update object*/
+            image.CssClass = "update-img";          /*the styling that is used for formatting the image*/
+
+            updateTitle.Text = update.title;                /*set the title of the update based on the update object*/
+            updateDescription.Text = update.description;    /*set the description of the update based on the update object*/
+            updateDate.Text = update.date.ToString("d");    /*set the date of the update based on the update object and format it to be short date ex. 2020-02-02*/
+
+            /*add the objects we created to the div in the order we want them to appear*/
             div.Controls.Add(image);
             div.Controls.Add(updateTitle);
             div.Controls.Add(updateDescription);
             div.Controls.Add(updateDate);
+
             return (div);
         }
 
