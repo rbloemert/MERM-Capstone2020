75a5f2852ef53c5f055847ea71951faa7bd25d22
rbloemert <56596601+rbloemert@users.noreply.github.com>
2021-02-14 20:32:28 -0500

    Worked on Creators.aspx
    Finally got grideview to work with displaying a list.

--- Project-Creator/Project-Creator/Creator.aspx.designer.cs
+++ Project-Creator/Project-Creator/Creator.aspx.designer.cs
@@ -13,5 +13,14 @@ namespace Project_Creator
 
     public partial class Creator
     {
+
+        /// <summary>
+        /// creatorProjectGrid control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView creatorProjectGrid;
     }
 }
