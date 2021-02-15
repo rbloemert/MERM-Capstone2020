4de9c3b040f66af650bd7050c803f78f4248694b
Mark <markjackson2112@gmail.com>
2021-02-14 19:54:27 -0500

    implemented shell for page. needs db connections.

--- Project-Creator/Project-Creator/ProjectManagement/ViewUpdate.aspx.cs
+++ Project-Creator/Project-Creator/ProjectManagement/ViewUpdate.aspx.cs
@@ -1,36 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
+﻿/*
+*	FILE : ViewUpdate.aspx.cs
+*	PROJECT : Project Creator
+*	PROGRAMMER : Mark Jackson
+*	FIRST VERSION : 2021-02-07
+*	DESCRIPTION :
+*		This webpage displays all of the information related to a singular update
+*		-it shows the update icon on the left
+*		-it shows the update information of the right
+*		-it shows links to the next and previous updates as well
+*/
+using System;
 using System.Web.UI.WebControls;
 
 namespace Project_Creator.ProjectManagement {
     public partial class ViewUpdate : System.Web.UI.Page {
-        private Update u;
-        protected void Page_Load(object sender, EventArgs e) {
-            string update = Session["Update"] as string;
-            string project = Session["Project"] as string;
+        private Update currentUpdate;   /*the information related to the update we want to display is stored here*/
 
+
+        /*
+        * METHOD : Page_Load
+        * DESCRIPTION :
+        *	when the page is first loaded, populate the update with information from the database
+        *	also draw the previous and next update links
+        * PARAMETERS :
+        *	object sender : the object that invoked the method
+        *   EventArgs e : addional arguments thata may be useful
+        * RETURNS :
+        *	none
+        */
+        protected void Page_Load(object sender, EventArgs e) {
+            string update = Session["Update"] as string;        /*the update we want to load up. it is passed from another screen*/
+            string project = Session["Project"] as string;      /*the project the update is from. it is passed from another screen*/
+
+            /*/if we did not get the information passed to us*/
             if (update == null || project == null) {
                 /*404*/
             } else {
                 /*get from db and populate page*/
+
+                /*since db doesn't exist yet generate update information to display*/
                 string img = "";
-                if (update == "002") {
+                if (project == "002") {
                     img = "https://pbs.twimg.com/profile_images/656441231560585216/NWOReGD5_400x400.jpg";
                 } else {
                     img = "https://cdn.escapistmagazine.com/media/global/images/library/deriv/1400/1400821.jpg";
                 }
-                u = new Update(img, "https://google.com", "<h3>Project Update Title</h3>", "<p>a description about the update which summarises the complete description that is displayed upon clicking on the card for more information.</p><br />", DateTime.Today);
+                currentUpdate = new Update(img, "https://google.com", "<h3>Project Update Title</h3>", "<p>a description about the update which summarises the complete description that is displayed upon clicking on the card for more information.</p><br />", DateTime.Today);
 
-                RelatedPanel.Controls.Add(createOtherPanel(u));
+                /*build elements to add to the content section*/
+                Label title = new Label();
+                title.Text = currentUpdate.title;
+                title.CssClass = "update-title";
+                Label content = new Label();
+                content.Text = currentUpdate.description;
+
+                /*add the content to the page*/
+                updateData.Controls.Add(title);
+                updateData.Controls.Add(content);
+
+                /*make the sidebar image the one from the update*/
+                sidebarImage.BackImageUrl = currentUpdate.iconURL;
+
+                /*add the current update to the update panel*/
+                RelatedPanel.Controls.Add(createOtherPanel(currentUpdate));
                 Panel div = new Panel();
-                div.CssClass = "w3-quarter";
+                div.CssClass = "timeline-content";
                 RelatedPanel.Controls.Add(div);
                 RelatedPanel.Controls.Add(div);
-                RelatedPanel.Controls.Add(createOtherPanel(u));
-
+                div = createOtherPanel(currentUpdate);
+                div.CssClass += " next-update";
+                RelatedPanel.Controls.Add(div);
             }
         }
 
@@ -39,7 +78,7 @@ namespace Project_Creator.ProjectManagement {
         /*
        * METHOD : createOtherPanel
        * DESCRIPTION :
-       *	
+       *	create the objects for the updates at the bottom of the screen
        * PARAMETERS :
        *	Update update : the update that we want to add
        * RETURNS :
@@ -51,17 +90,17 @@ namespace Project_Creator.ProjectManagement {
             Label nextPrev = new Label();
             Label updateTitle = new Label();        /*the label that will hold the title of the update*/
 
-            div.CssClass = "w3-quarter";             /*the styling that is used for formatting the div*/
+            div.CssClass = "timeline-content";             /*the styling that is used for formatting the div*/
 
             image.Click += new System.Web.UI.ImageClickEventHandler(updateClick);
             image.ID = update.updatePage;
             image.ImageUrl = update.iconURL;        /*set the image we want to use for the hyperlink. it is stored within the Update object*/
-            image.CssClass = "update-img";          /*the styling that is used for formatting the image*/
+            image.CssClass = "related-img";          /*the styling that is used for formatting the image*/
 
-            if(update.date > u.date) {
-                nextPrev.Text = "Next Update";
+            if(update.date > currentUpdate.date) {
+                nextPrev.Text = "<br>Next Update";
             } else {
-                nextPrev.Text = "Prev Update";
+                nextPrev.Text = "<br>Prev Update";
             }
             updateTitle.Text = update.title;                /*set the title of the update based on the update object*/
 
@@ -73,6 +112,20 @@ namespace Project_Creator.ProjectManagement {
             return (div);
         }
 
+
+
+        /*
+        * METHOD : updateClick
+        * DESCRIPTION :
+        *	when prev/next update is pressed this method is invoked
+        *	it navigates to somewhere. the current version is just a placeholder
+        * PARAMETERS :
+        *	object sender : the object that invoked the method
+        *   EventArgs e : addional arguments thata may be useful
+        * RETURNS :
+        *	none
+        *	we are navigated to another page
+        */
         protected void updateClick(object sender, EventArgs e) {
             Session["Update"] = ((ImageButton)sender).ID;
             Response.Redirect("ProjectTimeline.aspx");
