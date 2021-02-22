ddb609ce928f6bc4f227f3e5eaf780bce05bd083
Mark <markjackson2112@gmail.com>
2021-02-21 19:44:47 -0500

    added functionality to retrieve the timeline data from the database. there is currently a layout issue that has come up.

--- Project-Creator/Project-Creator/ProjectManagement/ProjectTimeline.aspx.designer.cs
+++ Project-Creator/Project-Creator/ProjectManagement/ProjectTimeline.aspx.designer.cs
@@ -40,13 +40,13 @@ namespace Project_Creator.ProjectManagement {
         protected global::System.Web.UI.WebControls.Label Label2;
 
         /// <summary>
-        /// lblAuthor control.
+        /// lblDesc control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblAuthor;
+        protected global::System.Web.UI.WebControls.Label lblDesc;
 
         /// <summary>
         /// UpdatePanel control.
