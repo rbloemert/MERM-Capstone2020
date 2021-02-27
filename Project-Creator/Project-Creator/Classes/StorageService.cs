cbfb642d7206e94a3d10bcd90d495dcd49af7002
maxmikheev5 <mikheev.maxim@gmail.com>
2021-02-27 05:07:45 -0500

    SharedFileStream interactivity with Azure Storage Account; NuGet pkg mgmt, db updates

---
+++ Project-Creator/Project-Creator/Classes/StorageService.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Azure;
+using Azure.Storage.Files.Shares;
+using Azure.Storage.Files.Shares.Models;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.File;
+
+namespace Project_Creator
+{
+    // Not sure what to call this, but this handles all file functionality
+    // https://docs.microsoft.com/en-us/azure/storage/blobs/storage-upload-process-images?tabs=dotnet
+    // This class is static, eliminating the need to create a new instance of the class when the class' methods are needed..
+
+    // If you get any errors here, you'll need the NuGet package 'Azure.Storage.File.Shares'
+    // Right click 'Project-Creator', select Manage NuGet packages
+
+    public class StorageService
+    {
+        // we should REALLY centralize connection strings somewhere, not leave them lying about!!!
+        private static string connectionString = "DefaultEndpointsProtocol=https;AccountName=projectcreatorstorage;AccountKey=8RPokT/yDpeXw67OPX0o/b2ml+mMMjdUVaOVfzq6CZtIOiN6OWaSiaEvsPQ1p3W84AdkWLap9WiuVZ8DOsVKZw==;EndpointSuffix=core.windows.net";
+        
+        // One of the following paths MUST precede file names:
+        // account_image
+        // project_image
+        // timeline_file
+        // timeline_image
+        //
+        // example of valid path: "project_image\image1.png"
+        // fileStream is a stream to the actual file iteself
+        // fileName is what the file name should be on the server
+        public static Response<ShareFileUploadInfo> UploadFileToStorage(Stream fileStream, string fileName)
+        {
+            ShareFileClient cl = new ShareFileClient(connectionString, "projectcreator", fileName);
+            return cl.Upload(fileStream); ;
+        }
+
+        public static async Task<bool> UploadFileToStorageAsync(Stream fileStream, string fileName)
+        {
+            ShareFileClient cl = new ShareFileClient(connectionString, "projectcreator", fileName);
+            await cl.UploadAsync(fileStream);
+            return await Task.FromResult(true);
+        }
+
+        // You can access the downloaded file through returned value.Content
+        public static Response<ShareFileDownloadInfo> DownloadFileFromStorage(string fileName)
+        {
+            ShareFileClient cl = new ShareFileClient(connectionString, "projectcreator", fileName);
+            return cl.Download();
+        }
+
+        // commented out until a good way to report both progress and return downloaded files is found; out params disallowed for async operations
+        //public static async Task<bool> DownloadFileFromStorageAsync(string fileName)
+        //{
+        //    ShareFileClient cl = new ShareFileClient(connectionString, "projectcreator", fileName);
+        //    await cl.DownloadAsync();
+        //    return await Task.FromResult(true);
+        //}
+
+        public static bool DoesFileExistOnStorage(string fileName)
+        {
+            ShareFileClient cl = new ShareFileClient(connectionString, "projectcreator", fileName);
+            return cl.Exists();
+        }
+
+    }
+}
--- Project-Creator/Project-Creator/Classes/Timeline.cs
+++ Project-Creator/Project-Creator/Classes/Timeline.cs
@@ -10,8 +10,9 @@ namespace Project_Creator
     {
         public int timelineID;                   /* timelineID: Unique timeline ID */
         public SqlDateTime timeline_creation;    /* timeline_creation: When this timeline was created. */
-        public string timeline_image_path;       /* timeline_image_path: Path of timeline image */
         public string timeline_name;             /* timelinetitle: The name of the timeline. */
         public string timeline_desc;             /* timelinedesc: The description of the timeline. */
+        public string timeline_image_path;       /* timeline_image_path: The File path of the timeline's picture; NULL means none */
+        public string timeline_file_path;        /* timeline_file_path: The File path of the timeline's file; NULL means none */
     }
 }
