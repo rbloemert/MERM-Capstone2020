dd9b2104454287f308c9ca8209c56e5efd8c3fc1
Mark <markjackson2112@gmail.com>
2021-02-28 16:55:01 -0500

    added element for comment_owner_accountID as one db methods already was retrieving it.

--- Project-Creator/Project-Creator/Classes/Comment.cs
+++ Project-Creator/Project-Creator/Classes/Comment.cs
@@ -11,5 +11,6 @@ namespace Project_Creator
         public int commentID;                   /* commentID: Unique ID of this comment. */
         public SqlDateTime comment_creation;    /* comment_creation: When this comment was created. */
         public string comment_text;             /* comment_text: The text of this comment. */
+        public string comment_owner_accountID;
     }
 }
