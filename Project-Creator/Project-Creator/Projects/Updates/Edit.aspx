5149937d159392ec3ab20ec95cc7189323e799b9
Mark <markjackson2112@gmail.com>
2021-03-13 20:50:40 -0500

    implemented first version of editing an update.

---
+++ Project-Creator/Project-Creator/Projects/Updates/Edit.aspx
@@ -0,0 +1,78 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Edit.aspx.cs" Inherits="Project_Creator.Projects.Updates.Edit" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link rel="stylesheet" href="../StyleSheets/StyleSheetFlickity.css">
+    <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceholder" runat="server">
+    <asp:Table Width="100%" runat="server">
+        <asp:TableRow>
+            <asp:TableCell>
+                <div class="ProjectTable">
+                    <div class="ProjectRow">
+                        <div class="ProjectColumn" style="width: 100%; max-width: 1014px;">
+                            <div class="Basic" style="height: 72px; margin-top: 4px; text-align: left">
+                                Update Title:
+                                <asp:TextBox ID="txtUpdate" CssClass="update-edit-textbox" Style="width: 90%" runat="server" />
+                                <br />
+                                Creation Date:
+                                <asp:Label ID="lblDate" runat="server" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </asp:TableCell>
+        </asp:TableRow>
+        <asp:TableRow>
+            <asp:TableCell>
+                <div class="ProjectTable">
+                    <div class="ProjectRow">
+                        <div class="ProjectColumn" style="width: 100%; max-width: 1014px;">
+                            <div class="Basic" style="margin: 0;">
+                                Update Image:
+                                <br />
+                                <asp:Image ID="TimelineImage" CssClass="timeline-image" runat="server" />
+                                <br />
+                                <asp:Button ID="btnNewImage" runat="server" OnClick="btnNewImage_Click" Text="Upload New Image" />
+                            </div>
+                            <div class="Basic" style="margin-top: 5px;">
+                                Update Description:
+                                <p>
+                                    <asp:TextBox ID="txtDesc" CssClass="update-edit-textbox" runat="server" onKeyUp="txtDesc_TextChanged()" MaxLength="255" ClientIDMode="Static"></asp:TextBox>
+                                    <script>
+                                        function txtDesc_TextChanged() {
+                                            val = document.getElementById("txtDesc").value;
+                                            document.getElementById("lblDescCounter").innerHTML = val.length + " of 255";
+                                        }
+                                    </script>
+                                    <asp:Label ID="lblDescCounter" runat="server" ClientIDMode="Static">0 of 255</asp:Label>
+                                </p>
+                            </div>
+                            <div class="Basic" style="margin-top: 5px;">
+                                Attatched File:
+                                <p>
+                                    <asp:TextBox ID="txtContent" CssClass="update-edit-textbox" runat="server" Text="Put attatched content here"></asp:TextBox>
+                                </p>
+                                <asp:Button ID="btnNewFile" runat="server" OnClick="btnNewFile_Click" Text="Upload New File" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </asp:TableCell>
+        </asp:TableRow>
+        <asp:TableRow>
+            <asp:TableCell>
+                <div class="ProjectTable">
+                    <div class="ProjectRow">
+                        <div class="ProjectColumn" style="width: 100%; max-width: 1014px;">
+                            <div class="Basic" style="margin: 0; text-align: right">
+                                <asp:Button ID="btnCancel" runat="server" OnClick="btnCancel_Click" Text="Cancel" />
+                                <asp:Button ID="btnSubmit" runat="server" OnClick="btnSubmit_Click" Text="Submit" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </asp:TableCell>
+        </asp:TableRow>
+    </asp:Table>
+</asp:Content>
---
+++ Project-Creator/Project-Creator/Projects/Updates/Edit.aspx.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Project_Creator.Projects.Updates {
+    public partial class Edit : System.Web.UI.Page {
+        public int ProjectID = 0;
+        public int UpdateID = 0;
+        public Project ProjectObject = new Project();
+        public Timeline TimelineObject = new Timeline();
+
+        protected void Page_Load(object sender, EventArgs e) {
+            //Gets the project id from the URL.
+            ProjectID = Convert.ToInt32(Request.QueryString["p"]);
+            UpdateID = Convert.ToInt32(Request.QueryString["u"]);
+
+            //Checks if the page is a post back.
+            if (!IsPostBack) {
+
+                Database db = new Database();
+                string ProjectAuthor = db.GetProjectAuthor(ProjectID);
+
+                //Checks if the project exists.
+                if (ProjectID != 0) {
+                    UpdateID = Convert.ToInt32(Request.QueryString["u"]);
+                    if(UpdateID != 0) {
+                        //Gets the database connection.
+                        //Checks if the user is logged in.
+                        if (Session["User"] != null) {
+
+                            //Gets the session user object.
+                            Account user = (Account)Session["User"];
+
+                            //Checks if the creator is the author of this project.
+                            if (user.username != ProjectAuthor) {
+
+                                //Redirects the user to the home page.
+                                Response.Redirect("/Home");
+
+                            } else {
+
+                                //Gets the project information.
+                                ProjectObject = db.GetProject(ProjectID);
+                                ProjectObject.project_author = db.GetProjectAuthor(ProjectID);
+
+                                TimelineObject = db.GetTimeline(UpdateID);
+
+                                //Sets the textbox to the project title.
+                                txtUpdate.Text = TimelineObject.timeline_name;
+                                lblDate.Text = TimelineObject.timeline_creation.ToString();
+                                TimelineImage.ImageUrl = TimelineObject.timeline_image_path;
+                                txtDesc.Text = TimelineObject.timeline_desc;
+                                lblDescCounter.Text = txtDesc.Text.Length + " of 255";
+                                //txtContent.Text = TimelineObject.timeline_file_path;
+                            }
+
+                        }
+
+                    } else {
+
+                        //Redirects the user to the home page.
+                        Response.Redirect("/Home");
+
+                    }
+
+                } else {
+
+                    //Checks if the user is logged in.
+                    if (Session["User"] != null) {
+
+                        //Gets the session user object.
+                        Account user = (Account)Session["User"];
+
+                        //Gets a project object.
+                        ProjectObject = new Project("New Project", "", user.username, "NULL");
+
+                        //Creates a new project to be editted.
+                        ProjectID = db.CreateProject(ProjectObject);
+
+                        //Links the project to the account.
+                        db.CreateProjectLink(ProjectID, user.accountID);
+
+                        //Updates the project ID.
+                        ProjectObject.projectID = ProjectID;
+
+                    } else {
+
+                        //Redirects the user to the home page.
+                        Response.Redirect("/Home");
+
+                    }
+                }
+
+            }
+
+        }
+
+        protected void btnNewImage_Click(object sender, EventArgs e) {
+            //TODO: Allow the user to upload an image
+        }
+
+        protected void btnNewFile_Click(object sender, EventArgs e) {
+            //TODO: Add a way for the user to add content
+        }
+
+        protected void btnCancel_Click(object sender, EventArgs e) {
+            Response.Redirect("View?p=" + ProjectID + "&u=" + UpdateID);
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e) {
+            TimelineObject.timeline_desc = txtDesc.Text;
+            TimelineObject.timeline_file_path = txtContent.Text;
+            TimelineObject.timeline_image_path = TimelineImage.ImageUrl;
+            TimelineObject.timeline_name = txtUpdate.Text;
+            TimelineObject.timeline_creation = Convert.ToDateTime(lblDate.Text);
+            TimelineObject.timeline_name = txtUpdate.Text;
+            Database db = new Database();
+            if(db.ModifyTimeline(UpdateID, TimelineObject) == Database.QueryResult.Successful) {
+                Response.Redirect("View?p=" + ProjectID + "&u=" + UpdateID);
+            }
+        }
+    }
+
+}
