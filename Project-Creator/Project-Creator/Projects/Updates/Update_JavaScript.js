b278e6015670741553e2f7692ee9a26056df2ed1
Mark <markjackson2112@gmail.com>
2021-04-05 00:19:36 -0400

    added a function for downloading and moved the document preview out due to having variable div names.

--- Project-Creator/Project-Creator/Projects/Updates/Update_JavaScript.js
+++ Project-Creator/Project-Creator/Projects/Updates/Update_JavaScript.js
@@ -18,49 +18,13 @@ function previewTimelineImage() {
     }
 }
 
-function previewFile() {
-    var preview = null;
-    const file = document.getElementById('ContentUploader').files[0];
-    const reader = new FileReader();
+const downloadToFile = (content, filename, contentType) => {
+    const a = document.createElement('a');
+    const file = new Blob([content], { type: contentType });
 
-    reader.addEventListener("load", function () {
-        // convert image file to base64 string
-        preview.src = reader.result;
-    }, false);
+    a.href = URL.createObjectURL(file);
+    a.download = filename;
+    a.click();
 
-    if (file) {
-        document.getElementById("<%=FileImage.ClientID %>").style.display = "none";
-        document.getElementById("<%=FileVideo.ClientID %>").style.display = "none";
-        document.getElementById("<%=FilePDF.ClientID %>").style.display = "none";
-        document.getElementById("<%=FileText.ClientID %>").style.display = "none";
-        var pdf = false;
-        switch (file.type) {
-            case ("image/jpeg"):
-            case ("image/png"):
-            case ("image/bmp"):
-                document.getElementById("<%=FileImage.ClientID %>").style.display = "block";
-                preview = document.getElementById("image_upload");
-                break;
-            case ("application/pdf"):
-                document.getElementById("<%=FilePDF.ClientID %>").style.display = "block";
-                preview = document.getElementById("viewer");
-                reader.readAsDataURL(file);
-                document.getElementById("wrapper").data = reader.result;
-                pdf = true;
-                break;
-            case ("video/mp4"):
-                document.getElementById("<%=FileVideo.ClientID %>").style.display = "block";
-                preview = document.getElementById("video_upload");
-                break;
-            case ("text/plain"):
-                document.getElementById("<%=FileText.ClientID %>").style.display = "block";
-                preview = document.getElementById("PDF_upload");
-                break;
-        }
-
-        if (pdf == false) {
-            reader.readAsDataURL(file);
-        }
-
-    }
-}
+    URL.revokeObjectURL(a.href);
+};
