9abaf6cce6cbe47eb5daf37294ac0f35f9991c6a
Mark <markjackson2112@gmail.com>
2021-04-19 11:18:12 -0400

    azure used Default.aspx as the home page. added a version that redirects to Home.aspx upon init so that default urls will redirect to home.

---
+++ Project-Creator/Project-Creator/Default.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Project_Creator {
+    public partial class Default : System.Web.UI.Page {
+        protected void Page_Init(object sender, EventArgs e) {
+            Response.Redirect("/Home");
+        }
+        protected void Page_Load(object sender, EventArgs e) {
+
+        }
+    }
+}
